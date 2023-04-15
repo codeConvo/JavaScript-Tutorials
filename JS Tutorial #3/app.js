84fdf2a60797d2596bd2f6ce09735ed8ad51d1f1
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-15 11:41:23 +0500

    JS lecture #3

---
+++ JS Tutorial #3/app.js
@@ -0,0 +1,21 @@
+let javaScriptisFun = false;
+
+console.log(typeof javaScriptisFun)
+
+let myVar
+console.log(myVar)
+
+
+let varString = "hello";
+
+console.log(typeof varString)
+
+let varNum = 12245;
+
+console.log(typeof varNum)
+
+myVar = 20;
+console.log(typeof myVar)
+
+let nullVar = null;
+console.log(typeof nullVar)
