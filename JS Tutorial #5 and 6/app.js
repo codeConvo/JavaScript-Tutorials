3c8e81c0a718f76767675d3e4e2c6d18f13394a9
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-17 12:21:17 +0500

    JS lecture #5 / 6

---
+++ JS Tutorial #5 and 6/app.js
@@ -0,0 +1,47 @@
+//******** Arithmetic Operators **********//
+
+// const now = 2023;
+// const ageByMe =now - 1997;
+// const ageByOther =now - 1998;
+
+// console.log(ageByMe,ageByOther)
+
+// console.log("CodeConvo" + " " + "WithBilal")
+
+// console.log(2 + 4, 4 - 2, 4 * 4, 5 / 5)
+
+
+// let value = 2 ** 5; // 2*2*2*2*2
+
+// console.log(value)
+
+//******** Assignment Operators **********//
+
+// let assign = 34;
+// assign += 20;
+// assign -= 20;
+// console.log(assign)
+
+// let AddValue = 50;
+// ++AddValue;
+// ++AddValue;
+// --AddValue
+// --AddValue
+// --AddValue
+// console.log(AddValue)
+
+//******** Comparison Operators **********//
+
+const now = 2023;
+const ageByBilal = now - 1997;
+const ageBySameer = now - 1998;
+
+// console.log(ageByMe > ageByOther)
+// console.log(ageByMe < ageByOther)
+
+console.log(ageByBilal, ageBySameer)
+// console.log(ageByBilal >= 18)
+// console.log(ageByBilal <= 18)
+
+// console.log(20 + 40 > 25 + 65)
+console.log((ageByBilal + ageBySameer) / 2)
