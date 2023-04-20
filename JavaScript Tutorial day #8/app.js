bac74464625a575d33dc5c3701fd9dd5de64537b
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-20 15:34:04 +0500

    JavaScript Tutorial day #8

---
+++ JavaScript Tutorial day #8/app.js
@@ -0,0 +1,16 @@
+const myName = "CodeConvo";
+const year = 2023;
+const dateOfBirth = 1997;
+const job = "programmer";
+
+const myDetails = " I am " + myName + " and a " + (year - dateOfBirth) + " old " + job;
+
+const myDetails2 = `I am ${myName} and a ${(year - dateOfBirth)} old ${job}`
+
+console.log(myDetails)
+console.log(myDetails2)
+
+
+console.log(`hello world
+how are you?
+welcome to ${myName}`)
