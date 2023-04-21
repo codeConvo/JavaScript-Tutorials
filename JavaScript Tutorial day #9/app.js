a3d1c07230eb586190068305f02fe50b30787587
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-21 17:19:42 +0500

    JavaScript Tutorial day #9

---
+++ JavaScript Tutorial day #9/app.js
@@ -0,0 +1,19 @@
+const age = 15;
+
+// const isYearOld = age >= 19;
+
+if( age >= 19){
+    console.log("Bilal Is ready to drive car🚗")
+}else{
+    const yearLeft = 19 - age;
+    console.log(`Bilal is to young wait ${yearLeft} `)
+}
+
+const years = 2012;
+let century;
+if(years <= 2000){
+    century = 20
+}else{
+    century = 21
+}
+console.log(century)
