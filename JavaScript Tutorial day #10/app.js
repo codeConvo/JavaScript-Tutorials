76ab07a1956ba1a374650e368ad047f8fe92c984
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-22 11:42:13 +0500

    JavaScript Tutorial day #10

---
+++ JavaScript Tutorial day #10/app.js
@@ -0,0 +1,28 @@
+// type conversion
+
+const st = "29";
+console.log(Number(st)+20 ,st)
+console.log(st +20)
+
+const string = "hello world"
+console.log(Number(string))
+
+
+const num = 50;
+console.log(String(num) +10)
+
+// coersion 
+
+const abc = "20"
+console.log(abc +10)
+
+console.log("I am " + 23 + "year old")
+
+console.log("40" / "30")
+
+let n = "10" +"20"
+n = n-1
+console.log(n)
+
+
+
