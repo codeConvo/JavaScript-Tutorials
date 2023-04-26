a86c9de02393281dbbe145f572b22da545c8ab80
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-26 16:33:40 +0500

    JavaScript Tutorial #14

---
+++ JavaScript Tutorial #14/app.js
@@ -0,0 +1,25 @@
+const day = 'monday';
+
+switch(day){
+    case 'monday': // day === 'monday'
+        console.log('week first working day');
+        // break;
+        case 'tuesday':
+        console.log('week second working day');
+        // break;
+        case 'wednesday':
+        console.log('week 3rd working day');
+        // break;
+        case 'thursday':
+        console.log('week 4th working day');
+        // break;
+        case 'friday':
+        console.log('week 5th working day');
+        break;
+        case 'saturday':
+        case 'sunday':
+        console.log('weekend funday');
+        break;
+      
+
+}
