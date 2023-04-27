0e0bd304ab21984ff096276ee931a617653bee61
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-27 15:26:46 +0500

    JavaScript Tutorial #15

---
+++ JavaScript Tutorial #15/app.js
@@ -0,0 +1,15 @@
+20 +30 ;
+1997;
+true && false || !false
+
+if(23 > 18){
+   const str = "statments" 
+}
+
+// switch(day){
+//     case 'mor':
+// }
+
+console.log(`my age is ${2023 -1997} ${if(23 > 18){
+    const str = "statments" 
+ }}`)
