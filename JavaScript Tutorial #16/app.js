7c95859d24a53699be4f02789e315fcb4bb9b07e
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-04-28 14:49:09 +0500

    JavaScript Tutorial #16

---
+++ JavaScript Tutorial #16/app.js
@@ -0,0 +1,22 @@
+const age  =15;
+
+age >= 18 ? console.log('Your start a driving') : console.log('Someone should drive a car');
+
+
+const drive = age >= 18 ? 'Your start a driving' :'Someone should drive a car'; 
+console.log(drive)
+
+console.log(`I am Bilal ${age >= 18 ? 'You start a driving' :'Someone should drive a car'} `)
+
+
+
+
+
+// let driv2;
+
+// if(age >= 18){
+//    driv2 = 'Your start a driving' 
+// }else{
+//    'Someone should drive a car'
+// }
+// console.log(driv2)
