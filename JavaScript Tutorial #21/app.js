24a6b9a9ca3be18c605974c095d11a9d73a9dee6
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-05-05 14:10:36 +0500

    JavaScript Tutorial #21

---
+++ JavaScript Tutorial #21/app.js
@@ -0,0 +1,35 @@
+// Array is a data structure that allow you to store a collection of values in a single variable
+
+const student1= 'bilal';
+const student2= 'Owais';
+const student3= 'haris';
+
+const students = ['Bilal' , 'Haris', "OWais"];
+console.log(students)
+
+console.log(students[0]);
+console.log(students.length);
+
+console.log(students[students.length-1])
+
+
+students[1] = 'sajid' 
+// students = ['sajid'];
+console.log(students);
+
+const friends = new Array(1997,2000,2020,2023);
+console.log(friends);
+
+
+const myName  = 'Syed Bilal ALI'
+const myDetail = [myName , 2023-1997 , "Progrmmer"];
+console.log(myDetail);
+
+function arr (birthday) {
+    return 2023 -birthday;
+}
+
+const years = [1997 ,2000 ,2020,2023];
+
+console.log(arr(years[1]));
+
