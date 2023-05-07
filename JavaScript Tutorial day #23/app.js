8146b2398590d6f78ea2d05c3b1c8282eb308f34
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-05-07 15:00:59 +0500

    JavaScript Tutorial day #23

---
+++ JavaScript Tutorial day #23/app.js
@@ -0,0 +1,18 @@
+const detailArr = [
+    'syed bilal',
+    'ali', 
+    'programmer',
+    2023 - 1997,
+    ['haris', 'sajjad', 'sajid']
+];
+console.log(detailArr[2])
+
+const detailObj = {
+    firstName: 'syed bilal',
+    lastName: 'Ali',
+    job:'Programmer',
+    age:2023-1997,
+    friends: ['haris', 'sajjad', 'sajid']
+}
+console.log(detailObj);
+console.log(detailObj.job);
