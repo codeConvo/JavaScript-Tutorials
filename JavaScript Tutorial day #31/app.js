0549200639305b9206c25b0fee46a4e6e82b64a6
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-05-17 15:57:54 +0500

    JavaScript Tutorial day #31

---
+++ JavaScript Tutorial day #31/app.js
@@ -0,0 +1,14 @@
+console.log(document.querySelector('.message').textContent);
+
+const labelScore = document.querySelector('.label-score').textContent = 40;
+console.log(labelScore)
+
+const num = document.querySelector('.number').textContent = 100;
+
+console.log(num);
+
+
+const labelHighscore = document.querySelector('.label-highscore').textContent = 20;
+
+const inp = document.querySelector('.text').value = 30
+console.log(inp)
