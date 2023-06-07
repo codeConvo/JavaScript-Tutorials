6a3077cae6a3e2b8ce58f32b9eab84663259040a
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-06-07 14:59:27 +0500

    JavaScript Tutorial day #47

---
+++ JavaScript Tutorial day #47/app.js
@@ -0,0 +1,59 @@
+// const openHours = {
+//     thus: {
+//         open: 11,
+//         close: 12
+//     },
+//     thus: {
+//         open: 11,
+//         close: 12
+//     },
+//     thus: {
+//         open: 11,
+//         close: 12
+//     },
+
+// }
+
+// const a = 'code';
+// const b = 'convo';
+
+// const obj = {
+//     a,
+//     b
+// }
+
+// console.log(obj);
+
+// const result = 'abc';
+
+// const obj1 = {
+//     [result]: "this is variable"
+// }
+
+// console.log(obj1.abc)
+
+
+
+// const restaurant = {
+//     name: 'Fast Food Hub',
+//     location: 'worldWide',
+//     category: ['fastfood', 'pizza', 'dessi', 'chiness'],
+//     staterMenu: ['biryani', 'pulao', 'zinger', 'sandwich', 'roll'],
+//     mainMenu: ['karhai', 'nihari', 'korma'],
+//     openHours,
+//     order(abc) {
+//         console.log(12);
+//     }
+// }
+// console.log(restaurant)
+
+const obj = {
+    x: 12,
+    y: 13,
+};
+
+function abc ({x,y}){
+    console.log(`x: ${x}, y: ${y}`)
+};
+
+abc(obj);
