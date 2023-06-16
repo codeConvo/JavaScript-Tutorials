96c99a2d56e68b39930c5aa56d32b20417d5b2e6
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-06-16 17:08:32 +0500

    JavaScript Tutorial day #52

---
+++ JavaScript Tutorial day #52/app.js
@@ -0,0 +1,16 @@
+const check = 'SYED BILAL ALI';
+const naam = 'Code Convo';
+
+console.log(check[1]);
+console.log(check[0]);
+console.log(check[2]);
+
+console.log(check.length);
+console.log(naam.indexOf('C'));
+console.log(naam.lastIndexOf('o'));
+
+console.log(naam.indexOf('Convo'));
+
+console.log(check.slice(4));
+
+console.log(check.slice(5,10))
