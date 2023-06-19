f1c24e250a701e5bcd73891f2f43a285987baefe
codeConvo684 <126857516+codeConvo@users.noreply.github.com>
2023-06-19 12:37:32 +0500

    JavaScript Tutorial day #53

---
+++ JavaScript Tutorial day #53/app.js
@@ -0,0 +1,31 @@
+const name = 'syed bilal ali';
+
+const channel = 'CODE CONVO';
+
+console.log(name.toUpperCase());
+
+console.log(channel.toLowerCase());
+
+'Jonas'
+
+// const userInp = prompt('please enter your username');
+
+// console.log(userInp);
+
+
+// const lower = userInp.toLowerCase();
+
+// const finalResult = lower[0].toUpperCase() + lower.slice(1);
+
+// console.log(finalResult);
+
+
+const email = 'abc@example.com';
+
+const loginEmail = '   aBc@example.com      ';
+
+console.log(email === loginEmail);
+
+const finalEmail = loginEmail.toLowerCase().trim()
+
+console.log(email === finalEmail);
